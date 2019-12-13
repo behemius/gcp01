500936ea2886948bc8e83568c17b2d2dcaf82d79
Krzysztof Wroblewski <behemius@gmail.com>
2019-12-13 20:14:00 +0100

    instance creation

---
+++ main.tf
@@ -0,0 +1,29 @@
+provider "google" {
+    project = "${var.gcp_project}"
+    region = "${var.gcp_region}"
+    zone = "${var.gcp_zone}"
+}
+
+resource "google_compute_instance" "vm_instance" {
+    name = "${var.instance_name}"
+    machine_type = "${var.machine_type}"
+    
+    scheduling {
+        automatic_restart   = true
+    }
+    
+    boot_disk {
+        initialize_params {
+            image = "debian-cloud/debian-9"
+            size = "10"
+            type = "pd-standard"
+        }
+    }
+
+    network_interface {
+        network = "default"
+        access_config {
+          // Ephemeral IP
+        }
+    }
+}
---
+++ variables.tf
@@ -0,0 +1,18 @@
+variable "gcp_project" {
+    default = "behemius"
+}
+variable "gcp_region" {
+    default = "europe-west3"
+}
+
+variable "gcp_zone" {
+    default = "europe-west3-c"
+}
+
+variable "instance_name" {
+    default = "mysql01"
+}
+
+variable "machine_type" {
+    default = "f1-micro"   
+}
