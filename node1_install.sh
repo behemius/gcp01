394c61c5efce023fcf127396961dee2ed2a47786
Krzysztof Wroblewski <behemius@gmail.com>
2019-12-17 20:25:50 +0100

    git change

--- node1_install.sh
+++ node1_install.sh
@@ -27,13 +27,6 @@ mysqlsh -e "dba.configureInstance('mycluster@mysql01',{password:'$password',inte
 mysqlsh -e "dba.configureInstance('mycluster@mysql02',{password:'$password',interactive:false,restart:true})"
 mysqlsh -e "dba.configureInstance('mycluster@mysql03',{password:'$password',interactive:false,restart:true})"
 
-<<<<<<< HEAD
-sleep 60 # waiting for nodes reconfiguration 
-
-mysqlsh mycluster@mysql01 --password=$password -e "dba.createCluster('mycluster')"
-mysqlsh mycluster@mysql01 --password=$password -e "cluster.addInstance('mycluster@mysql02:3306',{password:'$password'})"
-mysqlsh mycluster@mysql01 --password=$password -e "cluster.addInstance('mycluster@mysql03:3306',{password:'$password'})"
-=======
 sleep 30 # waiting as all nodes will be ready 
 
 mysqlsh mycluster@mysql01 --password=$password -e "dba.createCluster('mycluster',{ipWhitelist: '10.156.0.0/16'})"
@@ -42,4 +35,3 @@ sleep 30 # creation of cluster
 
 mysqlsh mycluster@mysql01 --password=$password -e "var cluster = dba.getCluster();cluster.addInstance('mycluster@mysql02:3306',{password:'$password',ipWhitelist: '10.156.0.0/16',interactive:false,recoveryMethod:'clone'});"
 mysqlsh mycluster@mysql01 --password=$password -e "var cluster = dba.getCluster();cluster.addInstance('mycluster@mysql03:3306',{password:'$password',ipWhitelist: '10.156.0.0/16',interactive:false,recoveryMethod:'clone'});"
->>>>>>> 4002e3e037db5e4a75498094aefec6e17fe02f5a
